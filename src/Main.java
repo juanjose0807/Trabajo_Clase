a93071cb5d6d0f0223686072bde3a2a594e846f5
juanjose.espinosa <juanjose.espinosa@udla.edu.ec>
2025-03-28 17:37:45 -0500

    Inicio

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import Models.Estudiante;
+import Models.Materia;
+
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,17 +9,32 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-       Estudiante est1 = new Estudiante();
-       est1.setNombre("Juan");
-       est1.setEdad(20);
-       est1.setNotas(8.5, 9.0, 7.5);
+        Scanner sc = new Scanner(System.in);
+        String nombre = sc.next();
+        int edad= sc.nextInt();
+        double nota1 = sc.nextDouble();
+        double nota2 = sc.nextDouble();
+        double nota3 = sc.nextDouble();
 
 
-        System.out.println("Nombre: " + est1.getNombre());
-        System.out.println("Edad: " + est1.getEdad());
-        double [] notas = est1.getNota1();
-        System.out.println("Notas: " + notas[0] + ", " + notas[1] + ", " + notas[2]);
+        Estudiante est1 = new Estudiante();
+       //est1.setNombre("Juan");
+        // est1.setEdad(20);
+       //est1.setNotas(8.5, 9.0, 7.5);
 
+
+       Materia mat1 = new Materia();
+         mat1.setNombre("Matematicas");
+         mat1.setCreditos(3);
+         mat1.setCodigo("MAT101");
+         mat1.setEstudiante(est1);
+
+         System.out.println("Nombre del estudiante: " + mat1.getEstudiante().getNombre());
+                 System.out.println("Edad del estudiante: " + mat1.getEstudiante().getEdad());
+                 double[] notas = mat1.getEstudiante().getNota1();
+                 System.out.println("Notas del estudiante: " + notas[0] + ", " + notas[1] + ", " + notas[2]);
 
         }
+
+
     }
--- src/Models/Materia.java
+++ src/Models/Materia.java
@@ -2,10 +2,41 @@ package Models;
 
 public class Materia {
 
+    private String nombre;
+    private int creditos;
+    private String codigo;
+    private Estudiante estudiante;
 
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    public int getCreditos() {
+        return creditos;
+    }
 
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
 
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
 }
